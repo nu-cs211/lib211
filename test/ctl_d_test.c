2ce208efa910ad32a8dbfa6a6b831e69d480c8bb
Jesse A. Tov <jesse.tov@gmail.com>
2020-02-06 13:06:42 -0600

    test for Ctl-D fake EOF working correctly.

---
+++ test/ctl_d_test.c
@@ -0,0 +1,19 @@
+#include <lib211.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s COUNT\n", argv[0]);
+        exit(1);
+    }
+
+    long limit = atol(argv[1]);
+
+    for (long i = 0; i < limit; ++i) {
+        char* line = read_line();
+        printf("%ld: %s\n", i, line ? line : "(null)");
+        free(line);
+    }
+}
